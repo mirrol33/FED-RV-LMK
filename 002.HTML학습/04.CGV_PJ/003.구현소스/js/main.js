9a930099c6398386fb04c8fa5c0c08c74cf0d277
freelife11 <mirrol33@gmail.com>
2024-11-27 12:01:58 +0900

    11.27 업데이트2

---
+++ 002.HTML학습/04.CGV_PJ/003.구현소스/js/main.js
@@ -0,0 +1,31 @@
+// CGV PJ 추가기능 JS - main.js
+
+// 로딩확인
+console.log("나야나 로딩!");
+
+// 화살표함수로 선택함수 만들기
+const qs = x => document.querySelector(x);
+const qsa = x => document.querySelectorAll(x);
+// 화살표함수로 이벤트 설정 함수 만들기
+const addEvt = (ele,evt,fn) => window.addEventListener(evt,fn);
+
+/**************************************************************** 
+[요구사항분석]
+- cgv 극장 예고편 선택 포스터 클릭시
+클릭된 li에 클래스 on을 넣고
+나머지 on을 제거처리한다
+- 결과적으로 css에 셋팅된 디자인이 적용됨
+1. 이벤트 종류 : click
+2. 이벤트 대상 : .poster ul > li
+3. 변경 대상 : 이벤트 대상과 동일
+****************************************************************/
+
+// 1. 대상선정 : 이벤트대상 + 변경대상
+// 1-1. 이벤트 대상 : .poster-menu ul > li
+const menu = qsa('.poster-menu ul > li');
+console.log("메뉴:", menu)
+// 1-2. 변경대상 : 이벤트 대상과 동일함
+// 2. 이벤트 설정하기
+// li가 여러개니까 개수만큼 for문을 돌림!
+// for(시;한;증){코드}
+// for(let i=0; i<개수; i++){코드}
