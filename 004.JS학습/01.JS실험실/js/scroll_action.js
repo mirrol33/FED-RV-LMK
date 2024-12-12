c65eb65bcd3ef257a4b26e2bdc49fef2fa30fe34
freelife11 <mirrol33@gmail.com>
2024-12-12 12:41:07 +0900

    12.12 업데이트1

--- 004.JS학습/01.JS실험실/js/scroll_action.js
+++ 004.JS학습/01.JS실험실/js/scroll_action.js
@@ -8,6 +8,10 @@ import callLetter from "./call_letter.js";
 
 // console.log(callLetter);
 
+// 부드러운 스크롤 함수 불러오기
+import startSS from "./smoothScroll23.js";
+startSS();
+
 // 글자등장함수 호출하기
 callLetter(".stage", "신카이 마코토", 1500);
 
@@ -109,8 +113,67 @@ function moveTit() {
   console.log("요리조리", scY);
 
   // 제일 큰값 기준부터 차례로 범위를 만들면 간단해진다!
-  if (scY >= posEl[2] - CRITERIA) tit.style.left = "30%"; // 3번째 요소
+  if (scY > posEl[2] - CRITERIA) tit.style.left = "30%"; // 3번째 요소
   else if (scY > posEl[1] - CRITERIA) tit.style.left = "78%";
-  else if (scY >= posEl[0] - CRITERIA) tit.style.left = "28%";
+  else if (scY > posEl[0] - CRITERIA) tit.style.left = "28%";
   else tit.style.left = "50%";
 }
+
+/****************************************************************** 
+    떨어지는 여자 기능 구현하기
+    _________________________
+
+    (1) 기본원리 : 스크롤 이동에 따른 화면 높이값 범위안에서 
+    떨어지는 여자 이미지가 아래쪽으로 이동 애니메이션 됨!
+
+    (2) 계산을 위한 비례식 세우기
+    스크롤 한계값 : 윈도우 높이값 = 스크롤 이동값 : 이미지 이동값
+
+    (3) 우리가 구할값은? 이미지 이동값!
+    -> 외항의 곱은 내항의 곱과 같다!
+    스한 : 윈높 = 스이 : 이이
+    스한 * 이이 = 윈높 * 스이
+    이이 = 윈높 * 스이 / 스한
+    
+******************************************************************/
+
+// 1. 변수값 셋팅하기
+// (1) 윈도우 높이값 (윈높)
+const winH = window.innerHeight;
+
+// 전체문서높이
+const docH = document.body.clientHeight;
+
+// (2) 스크롤 한계값 (스한) -> 전체문서높이 - 화면높이
+// 스한 = docH - winH
+const scLimit = docH - winH;
+console.log('문서높이:',docH, '\n윈도우높이:',winH,'\n스크롤한계값:',scLimit);
+
+// 2. 대상선정 : 떨어지는 여자요소
+const woman = myFn.qs('#woman');
+console.log('떨녀:',woman);
+
+// 3. 이벤트 설정하기 : window가 이벤트 대상임!
+myFn.addEvt(window, 'scroll',moveWoman);
+
+// 4. 함수만들기
+function moveWoman(){
+  // (1) 스크롤바 위치값 -> 스크롤 이동값
+  let scY = window.scrollY;
+
+  // (2) 떨녀 top값 구하기 : 이미지 이동값
+  // 이이 = 윈높 * 스이 / 스한
+  // 이이 = winH * scY / scLimit
+  
+  let womanTop = winH * scY / scLimit;
+  
+  // 호출확인 및 스크롤바 위치값, 떨녀 top값
+  console.log('스이:',scY,'\n이이:',womanTop);
+
+  // (3) 떨녀에게 적용하기
+  woman.style.top = womanTop + 'px';
+
+  // (4) 맨위일때 윗쪽으로 숨기기
+  if(scY < 50) woman.style.top = '-20%';
+} //// moveWoman ////
+
