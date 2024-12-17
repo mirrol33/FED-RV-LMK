c2ac5543a739cd5b7c5ae42f1dc0d84f846846c5
freelife11 <mirrol33@gmail.com>
2024-12-17 14:54:01 +0900

    12.17 업데이트2

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -4,13 +4,12 @@
 import slideFn from "./slide_fn.js";
 
 // 도깨비 PJ 데이터 불러오기
-import { previewData } from "../data/dkb_data.js";
+import {previewData} from "../data/dkb_data.js";
 
 console.log(previewData);
 
 // 1. 슬라이드함수 호출하여 실행하기
 slideFn();
-
 
 // 2. 데이터 셋팅하기 ////
 // (1) 미리보기 데이터 셋팅하기
@@ -24,13 +23,15 @@ slideFn();
 // -> 제이쿼리 html() 메서드에서는 join()없이
 // 콤마없애고 출력해줌!!!
 
-$('.preview-box ul').html(
-    previewData.map(v=>`
+$(".preview-box ul").html(
+  previewData.map(
+    (v) => `
         <li>
             <h3>${v.title}</h3>
             <p>${v.story}</p>
         </li>
-    `)
+    `
+  )
 );
 
 // document.querySelector('.preview-box ul')
@@ -41,3 +42,38 @@ $('.preview-box ul').html(
 //     </li>
 // `)
 // .join('')
+
+//스와이퍼 인스턴스 생성하기
+const swiper = new Swiper(".clip-box", {
+  // 한화면에 볼 슬라이드수
+  slidesPerView: 4,
+  // 슬라이드 사이 간격(숫자는 px단위임)
+  spaceBetween: 0,
+}); /// swiper /////
+
+// 스와이퍼 API를 이용한 개별 코딩
+// 내가 만든 버튼에 이동 기능 부여하기!
+const myBtns = document.querySelectorAll(".btn-box .abtn");
+console.log("대상:", myBtns);
+
+// 클릭이벤트 설정하기
+myBtns.forEach((el) => {
+  el.addEventListener("click", controlSwp);
+});
+
+// 기능구현함수
+function controlSwp() {
+  // 버튼구분하기
+  let isBtn = this.classList.contains("ab2");
+  console.log("나야나!", isBtn);
+
+  // 분기하기
+  // 다음버튼
+  if (isBtn) {
+    swiper.slideNext();
+  } // if //
+  // 이전버튼
+  else {
+    swiper.slidePrev();
+  } // else //
+} ////////// controlSwp함수 /////////
