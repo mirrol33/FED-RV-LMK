5689662cfcf67ce2e3fd92b8c11b15047949116c
freelife11 <mirrol33@gmail.com>
2024-12-19 15:02:07 +0900

    12.19 업데이트1

--- 002.HTML학습/07.ShoppingMall_PJ/js/main2.js
+++ 002.HTML학습/07.ShoppingMall_PJ/js/main2.js
@@ -34,6 +34,8 @@
 const $btnMove = $(".abtn");
 // 변경 대상: 슬라이드 박스(.slide)
 const $slide = $(".slide");
+// 변경 대상: 블릿 박스(.indic)
+const $indic = $(".indic li");
 
 // 광클금지 상태변수
 let stsClick = false;
@@ -96,6 +98,21 @@ $btnMove.click(function () {
       });
     }, 0);
   } /// else ///
+
+  // (3) 블릿 변경하기 : 해당순번에 클래스"on"넣기
+  // 대상: .indic li
+  // 순번은 슬라이드 방향별로 아래와 같이 읽어옴
+  // 아랫쪽 버튼은 두번째순번, 윗쪽버튼은 첫번째순번
+  let currIdx = $slide
+    .find("li")
+    .eq(isBtn ? 1 : 0)
+    .attr("data-seq");
+  console.log("읽은순번:", currIdx);
+
+  // 해당순번의 블릿li 클래스 "on"넣기 (나머지는 빼기)
+  $indic.eq(currIdx).addClass("on")
+  .siblings().removeClass("on");
+
 }); /// click ///
 
 // [3. 처음 슬라이드에 고유번호 속성넣기 ]
