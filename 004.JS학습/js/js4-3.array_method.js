e11b54e26796d0949e486a8eff96d826e561451e
freelife11 <mirrol33@gmail.com>
2024-12-30 14:12:47 +0900

    12.30 업데이트1

---
+++ 004.JS학습/js/js4-3.array_method.js
@@ -0,0 +1,124 @@
+// 배열의 메서드 활용 JS
+
+// 나의 함수 호출
+import myFn from "./my_function.js";
+
+/************************************************ 
+    [ 여기 등장하는 배열 메서드 정리 ]
+
+    1. push(값) - 뒷배열추가!
+    2. pop() - 뒷배열삭제!(뒷배열값 읽기)
+    3. unshift(값) - 앞배열추가!
+    4. shift() - 앞배열삭제!(앞배열값 읽기)
+    5. splice(순번,0,값) - 중간배열삽입!
+    6. splice(순번,개수) - 중간배열삭제!
+    _________________________________
+
+    7. join(구분자) - 배열값 구분자로 문자열변환!
+    8. map(v=>`<새값>${v}</새값>`) - 새배열!(배열리턴)
+    9. forEach(v=>{}) - 배열/유사배열 순회!
+    10. Object.keys(객체) - 객체의 키로 배열변환!
+    11. Object.values(객체) - 객체의 값으로 배열변환!
+    
+****************************************************/
+
+// 0. 기본정보 셋팅 //////////////////
+// (1) 배열변수 선언과 할당
+const fruit = ["배", "사과", "용과", "딸기"];
+
+// (2) 과일명과 배경이미지명을 매칭함 -> 객체
+const frObj = {
+  배: "fruits_01",
+  사과: "fruits_02",
+  용과: "fruits_14",
+  딸기: "fruits_09",
+  두리안: "fruits_17",
+  바나나: "fruits_15",
+  수박: "fruits_12",
+  파인애플: "fruits_13",
+  망고: "fruits_24",
+  오렌지: "fruits_03",
+  체리: "fruits_05",
+  멜론: "fruits_11",
+  블루베리: "fruits_20",
+  레몬: "fruits_04",
+  산딸기: "fruits_25",
+}; ////////// frObj 객체 /////
+
+// 1. 요구사항 :
+// 배열에 과일정보를 담아서 '과일주세요'버튼 클릭시
+// 과일 이미지요소를 화면에 출력해준다!
+// 배열구성을 수정, 삭제 등 배열 메서드로 변경할 수 있게 한다!
+
+// 2. 대상선정 :
+
+// 2-1. 이벤트 대상 : .mbtn (기능버튼들)
+const mbtn = myFn.qsa(".mbtn");
+
+// 2-2. 변경 대상 : .showit (배열정보출력) / .cont (과일출력박스)
+const showit = myFn.qs(".showit");
+const cont = myFn.qs(".cont");
+
+// 2-3. 전체과일콤보박스 : #sel
+const sel = myFn.qs("#sel");
+
+// 2-4. 선택과일콤보박스(anum=array number) : #anum
+const aNum = myFn.qs("#anum");
+
+// 2-5. 지울개수입력창 : #delnum
+const delNum = myFn.qs("#delnum");
+
+// console.log('대상:',mbtn,showit,cont,sel,aNum,delNum);
+
+// 3. 초기화 작업 : 처음배열 출력 / 콤보박스 바인딩
+
+// 3-1. 처음 배열 출력
+// - fruit 배열 변경시 다시 출력해야하므로 함수로 만들기
+const showArray = ()=>{
+  showit.innerHTML = fruit.join('♥');
+}; //// showArray ////
+
+// 처음배열출력함수 최초호출은 아랫쪽에서!!!
+showArray();
+
+// 4. 이벤트 설정하기
+//  -> 각 기능버튼에 클릭이벤트를 설정함
+mbtn.forEach(el=>{
+  myFn.addEvt(el,'click',showFruite);
+}) /// forEach ///
+function showFruite(){
+  
+  // (1) 버튼 텍스트 읽기
+  let btxt = this.innerText;
+  console.log(btxt);
+
+  // (2) 버튼별 기능 분기하기
+  // (2-1) '과일주세요~!' 버튼 : 하단박스에 과일이미지 출력
+  if(btxt === '과일주세요~!'){
+    // 출력대상: .cont -> cont 변수
+    cont.innerHTML = `
+    <ul>
+    ${
+      // fruit.map(v=>{return `<li>ㅋㅋ</li>`}).join('')
+      fruit.map(v=>`<li
+        style="background: url(./addimg/${frObj[v]}.png) no-repeat center / cover">${v}</li>`).join('')
+    }
+    </ul>
+    `;
+  } //// if ////
+  // (2-2) '뒷배열추가요~!' 버튼 : push() 메서드사용!
+  else if(btxt === '뒷배열추가요~!'){
+    fruit.push('멜론');
+    // 출력배열 업데이트함수 호출
+    showArray();
+  }//// else if ////
+  // (2-3) '뒷배열삭제요~!' 버튼 : pop() 메서드사용!
+  else if(btxt === '뒷배열삭제요~!'){
+    // let lastArr = fruit.pop();
+    // console.log('지우기찍기:',lastArr)
+    fruit.pop();
+    // 출력배열 업데이트함수 호출
+    showArray();
+  }//// else if ////
+
+} //// showFruite ////
