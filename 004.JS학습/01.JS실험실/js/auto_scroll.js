c6286a42e37d4db05a0ef8ef0fa7e48f7de7f96e
freelife11 <mirrol33@gmail.com>
2024-12-31 12:35:56 +0900

    12.31 업데이트1

---
+++ 004.JS학습/01.JS실험실/js/auto_scroll.js
@@ -0,0 +1,131 @@
+// 자동스크롤 JS - auto_scroll.js
+
+// 나의 함수 불러오기
+import myFn from "./my_function.js";
+
+/********************************************** 
+    [ 자동스크롤 기능정의 ]
+    1. 스크롤바가 없는 상태에서 또는 스크롤기능을 
+    막아놓은 상태에서 마우스 휠 작동시
+    아래와 같이 기능구현됨
+    (1) 휠 내림 : 다음페이지로 이동
+    (2) 휠 올림 : 이전페이지로 이동
+
+    2. 스크롤바 첫페이지와 끝페이지에서 이동안함
+
+    [ 자동스크롤 이벤트 ]
+    -> wheel 이벤트
+    -> 마우스 휠 작동시 발생!
+    (이전 이벤트명: 
+        mousewheel / DOMMouseScroll(파이어폭스))
+    (주의: wheel이벤트는 scroll이벤트와는 다름!
+        마우스의 바퀴를 돌릴때 발생함!)
+**********************************************/
+
+// 1. 자동스크롤 구현시 필요한 초기설정하기
+// (1-1) html에 부드러운 스크롤 설정
+myFn.qs("html").style.scrollBehavior = "smooth";
+// (1-2) body에 오버플로우 히든 설정
+myFn.qs("html").style.overflow = "hidden";
+
+// 2. 전역변수 설정하기
+// (2-1) 페이지변수
+let pgNum = 0;
+// (2-2) 휠상태변수 (광휠금지상태:true-막기,false-허용)
+let stsWheel = false;
+// (2-3) 각 페이지요소 수집 (.page인 요소들)
+const pageEl = myFn.qsa(".page");
+// (2-4) 전체 페이지수 상수
+const TOTAL_PAGE = pageEl.length;
+// console.log(pageEl, TOTAL_PAGE);
+
+// 3. 이벤트 등록하기
+// 대상 : window
+// - 전체 페이지 휠 이벤트 대상은 window다!
+// 이벤트종류 : wheel
+
+// myFn.addEvt(window, 'wheel', wheelFn);
+// window.addEventListener('wheel',wheelFn);
+window.addEventListener("wheel", wheelFn, {passive: false});
+
+// 4. 함수만들기
+function wheelFn(e) {
+  console.log("나야나!!!");
+
+  // (4-1) 기본기능막기
+  e.preventDefault();
+
+  // (4-2) 광휠막기
+  if (stsWheel) return;
+  stsWheel = true; // 잠금!
+  setTimeout(() => (stsWheel = false), 600); // 해제!
+
+  // (4-3) 휠방향 알아내기
+  // -> 델타값으로 알아낸다!
+  // 방향: 마이너스(아랫쪽), 플러스(윗쪽)
+  let delta = e.wheelDelta;
+  console.log(e.wheelDelta);
+
+  // (4-4) 방향 분기하여 전역 페이지변수 증감하기
+  if (delta < 0) pgNum++;
+  else pgNum--;
+
+  // (4-5) 한계값 체크 (0과 페이지끝번호 기준)
+  if (pgNum < 0) pgNum = 0;
+  else if (pgNum > TOTAL_PAGE - 1) pgNum = TOTAL_PAGE - 1;
+
+  console.log("pgNum", pgNum);
+
+  // (4-6) 페이지 이동하기
+  window.scrollTo(0, pageEl[pgNum].offsetTop);
+
+  // 이동원리 : scrollTo(x축위치값, y축위치값) 사용이동
+  // x축은 0, y축은 해당 순번의 .page박스 위치값 넣음
+  // pageEl[pgNum].offsetTop
+  // -> 페이지요소들[순번].위에서부터위치값
+} //// wheelFn 함수 ////
+
+/******************************************************* 
+    [ window / document / body 세가지는
+    기본막기불가 설정되어있음! ]
+    -> 이벤트 등록시 패지스모드가 true로 설정됨
+    셋팅방법:
+    요소.addEventListener(이벤트명,함수,{passive:true})
+    -> 기존엔  passive:false 였는데 이것의
+    기본값이 true로 변경됨. 의미는 기본기능막기
+    못하게 설정됨!
+
+    우리가 변경하여 사용해야함!
+    요소.addEventListener(이벤트명,함수,{passive:false})
+*******************************************************/
+
+// 5. 메뉴 클릭시 이동 추가기능 구현하기
+// (5-1) 대상선정: .gnb a
+const gnb = myFn.qsa(".gnb a");
+
+// (5-2) 이벤트설정
+gnb.forEach(el=>{
+    myFn.addEvt(el, "click", movePage);
+
+}); //// forEach ////
+
+// (5-3) 함수만들기
+function movePage(e) {
+  // (1) 기본이동막기
+  e.preventDefault();
+  
+  // (2) 클릭된 a요소의 href값 읽어오기(이동할 아이디)
+  // -> 요소속성값 가져오기 : getAttribute(속성명)
+  let tgId = this.getAttribute('href');
+  
+  // (3) 이동할 페이지 위치값 구하기
+  let pgPos = myFn.qs(tgId).offsetTop;
+  console.log("클릭!",this,tgId,pgPos);
+
+  // (4) 페이지 이동하기
+  window.scrollTo(0,pgPos);
+
+  // (5) 이동한 페이지
+
+
+} //// movePage ////
--- 004.JS학습/01.JS실험실/js/scroll_action.js
+++ 004.JS학습/01.JS실험실/js/scroll_action.js
@@ -10,7 +10,10 @@ import callLetter from "./call_letter.js";
 
 // 부드러운 스크롤 함수 불러오기
 import startSS from "./smoothScroll23.js";
+
+// 부드러운 스크롤 함수호출
 startSS();
+
 
 // 글자등장함수 호출하기
 callLetter(".stage", "신카이 마코토", 1500);
@@ -57,86 +60,96 @@ callLetter(".stage", "신카이 마코토", 1500);
 
 ****************************************************/
 
-// 1 대상선정
-// (1) 이벤트 대상: window
-// (2) 변경대상 .scroll-act
+// 1. 대상선정 :
+// (1) 이벤트 대상 : window
+// (2) 변경대상 : .scroll-act
 const scrollAct = myFn.qsa(".scroll-act");
-console.log(scrollAct);
-// (3) 변경대상: .tit
+// (3) 변경대상 : .tit
 const tit = myFn.qs(".tit");
-// 타이틀 요소에 트랜지션
+// 타이틀요소에 트랜지션
 tit.style.transition = ".4s ease-in-out";
+
+// console.log("대상:", scrollAct,tit);
 
 // 스크롤 등장요소의 위치값 담기
 // offsetTop은 맨위에서 부터 요소의 위치값
 // 배열변수에 순서대로 담는다!
 const posEl = [];
 scrollAct.forEach((el, idx) => (posEl[idx] = el.offsetTop));
+
 console.log("위치값:", posEl);
 
-// 2 이벤트 설정하기
+// 2. 이벤트 설정하기 ////////
 // (1) 스크롤시 요소등장 함수 호출
 myFn.addEvt(window, "scroll", showEl);
+
 // (2) 타이틀 요리조리 피하기 함수 호출
 myFn.addEvt(window, "scroll", moveTit);
 
-// 기준값 만들기 : 화면 높이값을 사용
-const CRITERIA = (window.innerHeight / 3) * 2; // 상수값
+// 기준값 만들기 : 화면 높이값을 사용(화면의 2/3)
+const CRITERIA = (window.innerHeight / 3) * 2;
 console.log("기준값:", CRITERIA);
 
-// 3 함수만들기
-// (1) 요소 등장 함수
+// 3. 함수만들기 /////////////
+// (1) 요소 등장 함수 /////////
 function showEl() {
   // (1) 함수호출확인
   // console.log('나야나!',window.scrollY);
 
   scrollAct.forEach((el) => {
-    // 테스트 : 첫번째 요소의 바운딩 top값
-    // console.log(scrollAct[0].getBoundingClientRect().top);
-    // console.log(myFn.getBCR(myscrollAct[0]));
-
     // 각 등장요소의 바운딩 top값
     let bcrVal = myFn.getBCR(el);
-    // console.log(bcrVal, scrollAct[0].getBoundingClientRect());
+    // console.log(bcrVal,
+    //     el.getBoundingClientRect());
 
-    //화면의 2/3위치에서 클래스 넣기(등장)
+    // 화면의 2/3위치에서 클래스 넣기(등장)
     if (bcrVal < CRITERIA) el.classList.add("on");
     // 기준값 전에는 다시 클래스 제거(원위치)
     else el.classList.remove("on");
-  });
-} //// showEl ////
-// (2) 요리조리 타이틀 움직이는 함수
+  }); ///// forEach /////
+} /////// showEl함수 ////////////////
+
+// (2) 요리조리 타이틀 움직이는 함수 ///
 function moveTit() {
   // 스크롤 위치값
   let scY = window.scrollY;
+  // console.log(scrollY);
 
-  console.log("요리조리", scY);
+  //(1) 함수호출확인
+  // console.log("요리조리!", scY);
 
   // 제일 큰값 기준부터 차례로 범위를 만들면 간단해진다!
-  if (scY > posEl[2] - CRITERIA) tit.style.left = "30%"; // 3번째 요소
-  else if (scY > posEl[1] - CRITERIA) tit.style.left = "78%";
-  else if (scY > posEl[0] - CRITERIA) tit.style.left = "28%";
+  if (scY > posEl[2])
+    // 3번째 요소
+    tit.style.left = "30%";
+  else if (scY > posEl[1])
+    // 2번째 요소
+    tit.style.left = "78%";
+  else if (scY > posEl[0])
+    // 1번째 요소
+    tit.style.left = "28%";
   else tit.style.left = "50%";
-}
+} //////// moveTit 함수 //////////////
 
-/****************************************************************** 
-    떨어지는 여자 기능 구현하기
-    _________________________
+/******************************************** 
+    [ 떨어지는 여자 기능 구현하기 ]
+    ______________________________
 
-    (1) 기본원리 : 스크롤 이동에 따른 화면 높이값 범위안에서 
-    떨어지는 여자 이미지가 아래쪽으로 이동 애니메이션 됨!
+    (1) 기본원리 : 스크롤 이동에 따른 화면
+    높이값 범위안에서 떨어지는 여자 이미지가
+    아래쪽으로 이동 애니메이션 됨!
 
     (2) 계산을 위한 비례식 세우기
-    스크롤 한계값 : 윈도우 높이값 = 스크롤 이동값 : 이미지 이동값
+      스크롤 한계값 : 윈도우 높이값 
+    = 스크롤 이동값 : 이미지 이동값
 
     (3) 우리가 구할값은? 이미지 이동값!
     -> 외항의 곱은 내항의 곱과 같다!
     스한 : 윈높 = 스이 : 이이
     스한 * 이이 = 윈높 * 스이
     이이 = 윈높 * 스이 / 스한
-    
-******************************************************************/
 
+********************************************/
 // 1. 변수값 셋팅하기
 // (1) 윈도우 높이값 (윈높)
 const winH = window.innerHeight;
@@ -147,33 +160,40 @@ const docH = document.body.clientHeight;
 // (2) 스크롤 한계값 (스한) -> 전체문서높이 - 화면높이
 // 스한 = docH - winH
 const scLimit = docH - winH;
-console.log('문서높이:',docH, '\n윈도우높이:',winH,'\n스크롤한계값:',scLimit);
+
+console.log(
+  "문서높이:",
+  docH,
+  "\n윈도우높이:",
+  winH,
+  "\n스크롤한계값:",
+  scLimit
+);
 
 // 2. 대상선정 : 떨어지는 여자요소
-const woman = myFn.qs('#woman');
-console.log('떨녀:',woman);
+const woman = myFn.qs("#woman");
+
+// console.log('떨녀:',woman);
 
 // 3. 이벤트 설정하기 : window가 이벤트 대상임!
-myFn.addEvt(window, 'scroll',moveWoman);
+myFn.addEvt(window, "scroll", moveWoman);
 
-// 4. 함수만들기
-function moveWoman(){
+// 4. 함수만들기 /////////
+function moveWoman() {
   // (1) 스크롤바 위치값 -> 스크롤 이동값
   let scY = window.scrollY;
 
   // (2) 떨녀 top값 구하기 : 이미지 이동값
   // 이이 = 윈높 * 스이 / 스한
   // 이이 = winH * scY / scLimit
-  
-  let womanTop = winH * scY / scLimit;
-  
+  let womanTop = (winH * scY) / scLimit;
+
   // 호출확인 및 스크롤바 위치값, 떨녀 top값
-  console.log('스이:',scY,'\n이이:',womanTop);
+  console.log("스이:", scY, "\n이이:", womanTop);
 
   // (3) 떨녀에게 적용하기
   woman.style.top = womanTop + 'px';
 
   // (4) 맨위일때 윗쪽으로 숨기기
   if(scY < 50) woman.style.top = '-20%';
-} //// moveWoman ////
-
+} //////////// moveWoman 함수 /////////////
