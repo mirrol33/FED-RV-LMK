040965621a030dc0671770d9238f09f2c36d841a
freelife11 <mirrol33@gmail.com>
2025-01-24 13:20:49 +0900

    01.24 update1

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/components/common.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/components/common.js
@@ -3,7 +3,7 @@
 // 1. 상단컴포넌트
 const TopComp = Vue.component("top-comp", {
   // 1-1. 템플릿코드설정 /////
-  template: `  
+  template: `
     <div id="top-area">
       <header class="top-area inbox">
         <!-- 1-1. 로고박스 -->
@@ -30,8 +30,28 @@ const TopComp = Vue.component("top-comp", {
             </li>
           </ol>
         </nav>
-      </header>      
-    </div>   
+        <!-- 1-4. 추가메뉴박스 -->
+        <nav class="add-menu">
+          <ol>
+            <li 
+            v-for="
+              (v,k) in this.addMenu
+              /* v - 객체값, k - 키명 */
+            "
+            :class="
+              // 키명이 '로그아웃'이면 'hide' 클래스넣기
+              k=='로그아웃' ? 'hide' : ''
+            ">
+              <a href="#"
+              @click.prevent="goPage(k)"
+              >
+              <i :class="v" :title="k"></i>
+              </a>
+            </li>
+          </ol>
+        </nav>
+      </header>
+    </div>
     `,
   // 1-2. 데이터 셋업 리턴 메서드 /////
   data() {
@@ -40,9 +60,49 @@ const TopComp = Vue.component("top-comp", {
       gnbMenu: ["FASHION", "BEAUTY", "LIFESTYLE", "CULTURE", "VIDEO"],
       // (2) 요약 메뉴 데이터
       sumMenu: ["KOREA", "구독하기", "≡"],
+      // (3) 추가가 메뉴 데이터 : 키는 메뉴, 값은 폰트어썸 클래스
+      addMenu: {
+        로그인: "fa-solid fa-right-to-bracket",
+        로그아웃: "fa-solid fa-right-from-bracket",
+        회원가입: "fa-solid fa-user",
+        장바구니: "fa-solid fa-cart-shopping",
+      },
     };
+  }, /// data ///
+  // 1-3. 컴포넌트 메서드구역 ////
+  methods: {
+    // 링크이동 메서드 : goPage
+    goPage(gubun) {
+      // gubun - 구분키(키명)
+      console.log(gubun);
+      // 페이지명 셋팅변수
+      let pgName;
+      // 구분키별 분기
+      switch (gubun) {
+        case "로그인":
+          pgName = "login";
+          break;
+        case "회원가입":
+          pgName = "member";
+          break;
+        case "장바구니":
+          pgName = "cart_list";
+          break;
+      } /// switch ///
+      // 페이지 이동하기 //
+      location.href = pgName + ".html";
+    }, /// goPage 메서드 ///
+  }, /// methods ///
+  // 1-4. 컴포넌트 라이프사이크 메서드: mounted
+  mounted() {
+    // 폰트어썸 link CSS 넣기
+    // $('head').prepend()
+    $("head").append(`
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+      `);
   },
-});
+}); //// TopComp ////
+
 // 2. 하단컴포넌트
 const BottomComp = Vue.component("bottom-comp", {
   // 템플릿 코드
