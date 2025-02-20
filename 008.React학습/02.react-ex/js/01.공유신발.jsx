2bfe50b27a817f8434bf658e34996f706f9fe8dc
freelife11 <mirrol33@gmail.com>
2025-02-20 13:20:44 +0900

    02.20 update2

--- 008.React학습/02.react-ex/js/01.공유신발.jsx
+++ 008.React학습/02.react-ex/js/01.공유신발.jsx
@@ -1,13 +1,13 @@
 // 01.공유신발 JSX
 
 // 상품리스트 서브컴포넌트 불러오기
-// import GoodsList from "./components/goods_list";
+import GoodsList from "./components/GoodsList";
 
 // 상품상세보기 서브컴포넌트 불러오기
-// import GoodsDetail from "./components/goods_detail";
+import GoodsDetail from "./components/GoodsDetail";
 
 // 공통함수 불러오기
-// import * as comFn from "./common/com_fn";
+import * as comFn from "./common/com_fn";
 
 // 주의사항!!! CDN에서 여기 import대상은 모두
 // html페이지에서 불러와야 사용할 수 있다!
@@ -22,30 +22,79 @@
 // 함수형 컴포넌트는 첫글자 대문자인 함수키워드로 만든다
 
 function MainComponent() {
+  // [ 후크상태관리변수셋팅!! ]
+  // 1. 리스트 / 상세보기 전환용 상태관리변수
+  const [ viewList, setViewList ] = React.useState(true);
+  // 2. 상품 데이터 인덱스값 상태관리변수
+  const [ gIdx, setGIdx ] = React.useState(1);
+  // 3. 선택 아이템 고유이름 상태관리변수
+  const [ selItem, selSelItem ] = React.useState("공유");
+  // 4. 테스트용 상태관리변수(의존성 테스트용!)
+  const [ test, selTest ] = React.useState(true);
+
   /************************************** 
-            [ 코드구성 ]
-            1. 타이틀 : h1.tit
-            2. 내용박스 : section
-            ㄴ> 제목 : h2
-            ㄴ> 이미지박스 : div.img-box > img
-            3. 기능버튼박스 : div.btn-box
-            ㄴ> 기능버튼 : button
-            4. 상품리스트박스 : div.gwrap
-            ㄴ> 상품리스트 : 
-                ul > li > ol > li > (img/text)
-            ㄴ> 상품상세보기 :
-                ol > li > (img/text/button)
-        **************************************/
+    [ 코드구성 ]
+    1. 타이틀 : h1.tit
+    2. 내용박스 : section
+    ㄴ> 제목 : h2
+    ㄴ> 이미지박스 : div.img-box > img
+    3. 기능버튼박스 : div.btn-box
+    ㄴ> 기능버튼 : button
+    4. 상품리스트박스 : div.gwrap
+    ㄴ> 상품리스트 : 
+        ul > li > ol > li > (img/text)
+    ㄴ> 상품상세보기 :
+        ol > li > (img/text/button)
+
+    __________________________________________
+
+    [ 일반 html 태그를 가져온 경우 수정사항 ]
+     1. 홀로태그는 반드시 끝에 스스로 닫기처리
+     예) <br> -> <br />
+     2. className 속성명은 classNameName으로 변경
+     3. 인라인 스타일은 객체형태로 변경
+      
+     
+
+**************************************************/
   ////////////////////////////////////
   // 코드리턴구역 /////////////////////
   return (
     <React.Fragment>
-        
+      <h1 className="tit">
+        <img
+          id="logo"
+          src="./images/logo.png"
+          alt="로고"
+          style={{
+            width: "50px",
+            verticalAlign: "-6px",
+            marginRight: "10px",
+          }}
+        />
+        <span>공유가 신고 다닌다는!</span>
+      </h1>
+      <section>
+        <h2 className="stit">공유는 오늘도 멋찝니다!</h2>
+        <div className="img-box">
+          <img src="./images/vans/gongyoo.jpg" alt="멋진공유" />
+        </div>
+      </section>
+      <div className="btn-box">
+        <button>효진초이스 바로가기</button>
+        <br />
+        <button>useEffect 의존성 테스트</button>
+      </div>
+      <div className="gwrap">
+        <GoodsList />
+        <GoodsDetail selItem="공유" gIdx="10" />
+      </div>
     </React.Fragment>
-);
+  );
 } ////////// MainComponent 컴포넌트 /////////////
 
 // 메인 컴포넌트 출력하기 ////////////
 ReactDOM.render(<MainComponent />, document.querySelector("#root"));
 // ReactDOM.render(어쩌구,저쩌구);
 // 어쩌구를 저쩌구에 출력해라!
+
---
+++ 008.React학습/02.react-ex/js/components/GoodsDetail.jsx
@@ -0,0 +1,84 @@
+/// [ 상품 상세보기 서브컴포넌트 : GoodsDetail ] ///
+
+// 공유신발 데이터 불러오기
+import guData from "../data/gu_data";
+// 효진드레스 데이터 불러오기
+import hjData from "../data/hj_data";
+// console.log(guData);
+
+export default function GoodsDetail({selItem, gIdx}) {
+  // selItem - 대분류(공유/효진) -> 데이터선택
+  // gIdx - 구체적인 선택 데이터 idx값
+  console.log("selItem", selItem, "\ngIdx", gIdx);
+
+  // 데이터 종류 선택하기 ////
+  const selDB = selItem === "공유" ? guData : selItem === "효진" ? hjData : null;
+
+  // 데이터 선택하기 ////
+  // selDB값이 null이면 false처리됨!
+  // Number(v.idx)===Number(gIdx)
+  // 위의 비교는 형까지 비교할때 에러를 방지하기 위함
+  const selData = selDB ? 
+  selDB.find((v) => {
+    if (Number(v.idx) === Number(gIdx)) return true
+  }) : "데이터가 없습니다!";
+
+  console.log("선택데이터:", selData);
+
+  // 리턴 코드구역 ////////////
+  return (
+    <ol
+      style={{
+        display: "flex",
+        listStyle: "none",
+        justifyContent: "center",
+      }}>
+      <li>
+        <img src={"./images/vans/vans_" + selData.idx + ".jpg"} alt="반스신발" style={{width: "100%"}} />
+      </li>
+      <li
+        style={{
+          lineHeight: "2",
+          padding: "10px",
+          textAlign: "left",
+        }}>
+        상품명 : {selData.gname}
+        <br />
+        가격 : {selData.gprice}
+        <br />
+        <div>
+          소재 : {selData.소재}
+          <br />
+          색상 : {selData.색상}
+          <br />
+          치수 : {selData.치수}
+          <br />
+          제조자/수입자 :{selData["제조자/수입자"]}
+          <br />
+          제조국 : {selData.제조국}
+          <br />
+          제조연월 : {selData.제조연월}
+          <br />
+          A/S 책임자와 전화번호 : <br />
+          {selData["A/S 책임자와 전화번호"]}
+          <br />
+          Model : {selData.Model}
+          <br />
+        </div>
+        <div
+          className="btnbx"
+          style={{
+            textAlign: "right",
+            padding: "15px",
+          }}>
+          <button
+            style={{
+              fontSize: "24px",
+            }}>
+            리스트로 가기
+          </button>
+        </div>
+      </li>
+    </ol>
+  );
+} //////////// GoodsDetail 컴포넌트 /////////
