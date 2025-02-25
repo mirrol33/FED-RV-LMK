0f47b324dd849d62cd16db096dafe7ff49218f98
freelife11 <mirrol33@gmail.com>
2025-02-25 15:01:13 +0900

    02.25 update4

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/Banner.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/Banner.jsx
@@ -1,26 +1,68 @@
 // DC.com 배너 컴포넌트 - Banner.jsx ///////
 import React from "react";
 
-// 배너용 CSS 불러오기 ///
-import '../../css/modules/banner.scss';
+// 배너 데이터 불러오기 ///
+import {banData} from "../../js/data/banner";
 
-function Banner(props) {
+// 배너용 CSS 불러오기 ///
+import "../../css/modules/banner.scss";
+
+// 배너 슬라이드 기능 함수 불러오기 ///
+import SlideFn from "../../js/func/go_slide";
+
+
+function Banner({catName}) {
+  // catName - 배너 데이터 카테고리 이름
+
+  // 슬라이드 기능 생성자함수 인스턴스 생성하기
+  const slideFn = new SlideFn();
+
+  // 선택데이터 ////
+  const selData = banData[catName];
+
   return (
     <>
       <div className="banner">
+        {/* 슬라이드 리스트 */}
         <ul className="slider">
-          <li>
-            <img
-              src="./images/dcm21.jpg"
-              alt="GOTHAM GAZETTE"
-            />
-            <section className="bantit">
-              <h2>GOTHAM GAZETTE</h2>
-              <p>WORLDS TRAVELER</p>
-              <button>New Places, Familiar Faces</button>
-            </section>
-          </li>
+          {
+            // 배열데이터 개수 만큼 슬라이드 생성하기
+            selData.map((v, i) => (
+              <li key={i}>
+                <img src={v.src} alt={v.tit1} />
+                <section className="bantit">
+                  <h2>{v.tit1}</h2>
+                  <p>{v.tit2}</p>
+                  <button>
+                    55
+                    {v.btn}
+                  </button>
+                </section>
+              </li>
+            ))
+          }
         </ul>
+        {
+          //슬라이드배열개수가 1초과일때만 나오기
+          //양쪽이동버튼 + 블릿표시자
+          selData.length > 1 && (
+            <>
+              {/* 양쪽이동버튼 */}
+              <button className="abtn lb"
+              onclick={slideFn.goSlide}
+              >＜</button>
+              <button className="abtn rb"
+              onclick={slideFn.goSlide}
+              >＞</button>
+              {/* 블릿 표시자 */}
+              <ol className="indic">
+                {selData.map((v, i) => (
+                  <li key={i} className={i === 0 ? "on" : ""}></li>
+                ))}
+              </ol>
+            </>
+          )
+        }
       </div>
     </>
   );
