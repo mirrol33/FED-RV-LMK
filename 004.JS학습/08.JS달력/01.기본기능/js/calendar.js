c87f7d01902d2cc2d250c803f2f304a2e0561801
freelife11 <mirrol33@gmail.com>
2025-02-25 11:21:16 +0900

    02.25 update2

--- 004.JS학습/08.JS달력/01.기본기능/js/calendar.js
+++ 004.JS학습/08.JS달력/01.기본기능/js/calendar.js
@@ -1,16 +1,16 @@
 // 달력구현 JS - calendar.js ////////////
 
 // DOM 메서드 //////
-const dFn = {
+const myFn = {
     qs : x => document.querySelector(x),
     qsa : x => document.querySelectorAll(x),
     qsEl: (el, x) => el.querySelector(x),
     qsaEl: (el, x) => el.querySelectorAll(x),
     addEvt : (ele,evt,fn) => 
             ele.addEventListener(evt,fn),
-    cg : x => console.log(x),
+    cs : x => console.log(x),
     addZero : x => x < 10 ? '0' + x : x,
-    fm : x => `${x.getFullYear()}-${
+    dfm : x => `${x.getFullYear()}-${
         dFn.addZero(x.getMonth()+1)}-${
         dFn.addZero(x.getDate())}(${week[x.getDay()]})`
 }; ///////// dFn 객체 //////////
@@ -22,6 +22,28 @@ const week = ["일","월","화","수","목","금","토"];
 makeDallyeok();
 
 function makeDallyeok(){
-    dFn.cg('달력만들어!');
+    myFn.cs('달력만들어!');
 
 } /////////////// makeDallyeok함수 ////////////
+
+    // 1. 변수셋팅 ////////////////////
+    // (1) 변경할 현재날짜 객체
+    const currDate = new Date();
+    // (2) 오늘날짜 객체
+    const today = new Date();
+    // (3) 년도요소 : .yearTit
+    const yearTit = myFn.qs('.yearTit');
+    // (4) 월요소 : .monthTit
+    const monthTit = myFn.qs('.monthTit');
+    // (5) 날짜요소 : .dates
+    const dates = myFn.qs('.dates');
+    // (6) 날짜넣을 배열변수
+    const dateSet = [];
+    // (7) html 코드 저장변수
+    let hcode = '';
+
+    // 2. 함수 만들기
+    // (1) 달력 초기화 구성함수 ////
+    const initDallyeok = () => {
+
+    }; //// initDallyeok ////
