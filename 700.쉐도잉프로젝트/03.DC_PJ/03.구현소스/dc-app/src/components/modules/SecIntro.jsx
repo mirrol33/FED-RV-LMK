fba3cb59f5ef3c28f22307f31ca17df6121e991f
freelife11 <mirrol33@gmail.com>
2025-02-26 17:01:32 +0900

    02.26 update6

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/SecIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/SecIntro.jsx
@@ -1,12 +1,39 @@
 // DC.com 섹션소개 컴포넌트 - SecIntro.jsx ///
-import React from 'react';
+import React from "react";
+
+// 섹션 소개 모듈용 데이터 불러오기
+import {secIntroData} from "../../js/data/sec_intro";
+
+// 섹션 소개 모듈 CSS 불러오기
+import "../../css/modules/sec_intro.scss";
+import {Link} from "react-router-dom";
 
 function SecIntro(props) {
-    return (
-        <div>
-            <h1>나는 섹션소개 파트야!</h1>
+  // 불러온 데이터 변수할당
+  const selData = secIntroData;
+
+  // 리턴 코드구역
+  return (
+    <section className="sec-intro">
+      {/* 반복단위박스 */}
+      {selData.map((v, i) => (
+        <div key={i}>
+          <div className="imbx">
+            <img src={v.isrc} alt={v.tit.split("^")[0]} />
+          </div>
+          <div className="titbx">
+            <h3>{v.tit.split("^")[0]}</h3>
+            <h2>{v.tit.split("^")[1]}</h2>
+          </div>
+          <div className="btnbx">
+            <Link to={v.link}>
+              <button>{v.btn}</button>
+            </Link>
+          </div>
         </div>
-    );
+      ))}
+    </section>
+  );
 }
 
 export default SecIntro;
