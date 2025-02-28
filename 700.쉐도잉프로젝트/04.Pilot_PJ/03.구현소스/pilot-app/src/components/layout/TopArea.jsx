fd4d26c66d56b0637b1671075e99f45bcc8d1962
freelife11 <mirrol33@gmail.com>
2025-02-28 17:06:46 +0900

    02.28 update3

--- 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/layout/TopArea.jsx
@@ -1,10 +1,51 @@
 /// 상단영역 컴포넌트 : TopArea.jsx /////
 
+// GNB 데이터 가져오기 ////
+import { gnbData } from "../../js/data/gnb";
+
+// 전체메뉴 컴포넌트 불러오기
+
 export default function TopArea() {
+
+   /// GNB메뉴 리스트 만들기 함수
+   const makeList = (dataName) => {
+    console.log('데이터이름:',dataName);
+    return(
+      gnbData[dataName].map((v,i)=>
+        <li key={i}
+          className={i===0?'on':''}>
+          <a href={"#c"+(i+1)}>{v}</a>
+        </li>
+      )
+    )
+
+  }; ///////// makeList /////////
+
   /// 리턴 코드구역 ////////
   return (
     <>
-    <h1>상단영역</h1>
+      <div id="top-area">
+        <header className="top-area ibx">
+          <h1 id="logo">
+            <a href="#">
+              <img
+                src="/images/main_logo.png"
+                alt="파일럿로고"
+              />
+            </a>
+          </h1>
+          <nav className="gnb">
+            <ul>
+              <li className="bld">배너순번 li 숨기기</li>
+              {makeList('main')}
+            </ul>
+          </nav>
+          <div className="ham">
+            <span></span> <span></span> <span></span>
+          </div>
+          {/* 전체메뉴 컴포넌트 */}
+        </header>
+      </div>
     </>
   );
 } //////////// TopArea 컴포넌트 ///////////
--- 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/index.js
@@ -1,8 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import Main from "./components/pages/Main";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// 공통 CSS 불러오기 ///
+import "./css/index.scss";
+
+import Layout from './components/layout/Layout';
+import Main from './components/pages/Main';
+
 
 /// 전체 PJ 공통 CSS 최상위 JS에서 불러오기 ///
 // import "./css/index.scss";
@@ -10,24 +15,27 @@ import Main from "./components/pages/Main";
 // 리액트 import에서는 모두 정확히 써야함!
 
 //// 메인 컴포넌트 ///////////////////////////////
-export default function MainComponent() {
-  // 리턴 코드구역 ////////////
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* 최상위 Route는 쌍으로 태그를 만든다!
+export default function MainComponent(){
+
+    // 리턴 코드구역 ////////////
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* 최상위 Route는 쌍으로 태그를 만든다!
                 슬래쉬는 루트를 말하고 레이아웃 컴포넌트 불러옴 */}
-        <Route path="/" element={<Layout />}>
-          {/* 하위중 첫페이지는 index라고 속성씀! */}
-          <Route index element={<Main />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+                <Route path="/" element={<Layout />}>
+                {/* 하위중 첫페이지는 index라고 속성씀! */}
+                    <Route index element={<Main  />}  />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+
 } /////////// MainComponent ////////////////////
 
 /// 컴포넌트 출력 ///
 // 먼저 root 객체 만들기
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+const root = ReactDOM.createRoot(
+    document.querySelector("#root"));
 // 출력하기
 root.render(<MainComponent />);
