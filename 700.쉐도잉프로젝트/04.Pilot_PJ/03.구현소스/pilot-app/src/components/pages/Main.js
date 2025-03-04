92707904553fcf786e615e6cac6c3cec8ea36f9d
freelife11 <mirrol33@gmail.com>
2025-03-04 16:21:44 +0900

    03.04 update2

--- 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/Main.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/Main.js
@@ -1,20 +1,63 @@
 // Pilot PJ 메인 페이지 컴포넌트 - Main.jsx
 
-import React from "react";
+import React, { useEffect } from "react";
 
-function Main(props) {
+// 자동스크롤 기능 JS 불러오기
+import * as autoFn from "../../js/func/jquery-autoScroll";
+
+// 드래그배너 기능 JS 불러오기 ////
+import { dragBanner } from "../../js/func/drag_banner";
+
+import { Banner } from "../modules/Banner";
+import { FashionIntro } from "../modules/FashionIntro";
+
+function Main() {
+    // 컴포넌트 로딩후 실행구역 : 한번만 (빈의존성[])
+    useEffect(()=>{
+
+        // 자동스크롤 이벤트 설정하기
+        window.addEventListener('wheel', autoFn.wheelFn);
+        // window 이벤트 설정을 여기서한 이유는?
+        // -> 소멸자를 통해 이벤트를 다른 페이지가 나올때
+        // 해제해 주기 위해 여기서 셋팅함!
+
+        // 메뉴+인디케이터 이벤트 기능 설정함수 호출
+        autoFn.evtFn();
+
+        // 초기화 함수 호출
+        autoFn.initSet();
+
+        // 페이지번호 초기화 함수 호출
+        autoFn.zeroPno();
+
+        // 드래그배너 기능함수 호출하기
+        dragBanner();
+
+    },[]); //// useEffect : 한번만 ////
+
+
   return (
     <>
       {/* 1. 배너 페이지 */}
-      <section id="ban" className="page none-sel" style={{background: "lightblue"}}></section>
+      <section id="ban" className="page none-sel" style={{background: "lightblue"}}>
+
+            <Banner />
+
+      </section>
       {/* 2. 남성패션 페이지 */}
-      <section className="page"></section>
+      <section className="page">
+        <FashionIntro cat="men" subcat="etc" />
+      </section>
 
       {/* 3. 여성패션 페이지 */}
-      <section className="page"></section>
+      <section className="page">
+        <FashionIntro cat="women" subcat="etc" />
+      </section>
 
       {/* 4. 스타일패션 페이지 */}
-      <section className="page"></section>
+      <section className="page">
+        <FashionIntro cat="style" subcat="etc" />
+      </section>
 
       {/* 메인에만 나오는 사이드 인디케이터 */}
       <nav className="indic">
