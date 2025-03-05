9aa9c8997f43415f75c02d1bfd9cab48cc2fe661
freelife11 <mirrol33@gmail.com>
2025-03-05 14:19:37 +0900

    03.05 update5

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -1,23 +1,34 @@
 /// 상단영역 컴포넌트 : TopArea.jsx /////
 
-import { Link } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 // GNB 데이터 불러오기 ////////
-import { menu } from "../../js/data/gnb";
+import {menu} from "../../js/data/gnb";
 
 // 상단영역 CSS 불러오기 ///
 import "../../css/common/top_area.scss";
 import Logo from "../modules/Logo";
 
 // 폰트어썸 불러오기 ////
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 // 돋보기 아이콘 ///
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
 // 제이쿼리 불러오기 ////
 import $ from "jquery";
 
 export default function TopArea() {
+  // [ 라우터 이동함수 객체 생성하기 ]
+  const goPage = useNavigate();
+  // 사용시 goPage(라우터주소, {전달객체})
+  // 전달객체가 없으면 쓰지 않는다!
+  // 사용법 : 반드시 useNavigate() 생성자메서드를 변수에 할당
+  // 이동할 라우터 주소를 쓰면 이동함!
+  // 예) goPage('/news') -> 뉴스페이지로 이동
+  // 예) gpPage('/') -> 첫페이지로 이동
+  // 예) gpPage('') -> 첫페이지로 이동
+  // -> 이동주소는 대소문자 구분하지 않는다!
+  // -> 슬래쉬없이 빈값을 써도 루트로 이동함!
 
   // [ 검색 관련 함수들 ] ///////
   // 1. 검색창 보이기함수 ////
@@ -25,10 +36,9 @@ export default function TopArea() {
     // 기본기능막기
     e.preventDefault();
     // 검색입력박스 보이기
-    $('.searchingGnb').show();
+    $(".searchingGnb").show();
     // 2. 입력창에 포커스 보내기
-    $('#schinGnb').focus();
-
+    $("#schinGnb").focus();
   }; //////////// showSearch 함수 //////////
 
   // 2. 검색창에 엔터키 누르면 검색함수 호출함수
@@ -37,31 +47,31 @@ export default function TopArea() {
     // console.log(e.key, e.keyCode);
 
     // 엔터키일 경우 입력값 읽어서 검색함수호출하기
-    if(e.key === "Enter"){
+    if (e.key === "Enter") {
       // (1) 입력창의 입력값 읽어오기 : val() 사용!
       let txt = $(e.target).val().trim();
       console.log(txt);
       // (2) 빈값이 아니면 검색함수 호출
-      if(txt !== ''){
+      if (txt !== "") {
+        // 입력창 비우고 부모박스 닫기
+        $(e.target).val('').parent().hide();
+        // 검색 보내기
         goSearch(txt);
       } /// if ///
       // (3) 빈값이면 메시지 출력
-      else{
-        alert('Please write letters for Searching!');
+      else {
+        alert("Please write letters for Searching!");
       } /// else ///
-
     } /// if ///
-
   }; //////////// enterKey 함수 ////////////
 
   // 3. 검색페이지로 검색어와 함께 이동하기 함수
   const goSearch = (txt) => {
-    console.log('나는 검색하러 간다규~!',txt);
+    console.log("나는 검색하러 간다규~!", txt);
     // 라우터 이동함수로 검색페이지로 이동하기!
-
+    goPage("search", {state: {keyword: txt}});
+    // 네비게이트 메서드(라우터주소, {state:{보낼객체}})
   }; /////////// goSearch 함수 /////////////
-
-
 
   /// 리턴 코드구역 ////////
   return (
@@ -115,25 +125,13 @@ export default function TopArea() {
               style={{
                 marginLeft: "auto",
                 marginRight: "25px",
-              }}
-            >
+              }}>
               {/* 검색입력박스 */}
               <div className="searchingGnb">
                 {/* 검색버튼 돋보기 아이콘 */}
-                <FontAwesomeIcon 
-                  icon={faSearch}
-                  className="schbtnGnb"
-                  title="Open search"
-                  onClick={()=>{}}
-                />
+                <FontAwesomeIcon icon={faSearch} className="schbtnGnb" title="Open search" onClick={() => {}} />
                 {/* 입력창 */}
-                <input 
-                  type="text" 
-                  name="schinGnb"
-                  id="schinGnb"
-                  placeholder="Filter by Keyword"
-                  onKeyUp={enterKey}
-                />
+                <input type="text" name="schinGnb" id="schinGnb" placeholder="Filter by Keyword" onKeyUp={enterKey} />
               </div>
               {/* 검색기능링크 - 클릭시 검색창 보이기 */}
               <a href="#" onClick={showSearch}>
