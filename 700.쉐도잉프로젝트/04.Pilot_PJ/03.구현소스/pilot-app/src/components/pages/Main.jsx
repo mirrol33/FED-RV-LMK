f7df6d1b3af8a887e38f71c80a181e493dfad2fd
freelife11 <mirrol33@gmail.com>
2025-03-06 16:02:22 +0900

    03.06 update2

--- 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/Main.jsx
@@ -1,50 +1,66 @@
 // Pilot PJ 메인 페이지 컴포넌트 - Main.jsx
 
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 
 // 자동스크롤 기능 JS 불러오기 ///
 import * as autoFn from "../../js/func/jquery-autoScroll";
 
 // 드래그배너 기능 JS 불러오기 ////
-import { dragBanner } from "../../js/func/drag_banner";
+import {dragBanner} from "../../js/func/drag_banner";
 
-import { Banner } from "../modules/Banner";
-import { FashionIntro } from "../modules/FashionIntro";
+import {Banner} from "../modules/Banner";
+import {FashionIntro} from "../modules/FashionIntro";
+
+// 제이쿼리 불러오기
+import $ from "jquery";
 
 function Main() {
-    // 컴포넌트 로딩후 실행구역 : 한번만 (빈의존성[]) /////
-    useEffect(()=>{
+  // 컴포넌트 로딩후 실행구역 : 한번만 (빈의존성[]) /////
+  useEffect(() => {
 
-        // 자동스크롤 이벤트 설정하기
-        window.addEventListener('wheel', autoFn.wheelFn);
-        // -> window 이벤트 설정을 여기서한 이유는?
-        // ->>> 소멸자를 통해 이벤트를 다른 페이지가 나올때
-        // 해재해 주기 위해 여기서 셋팅함!
+    // 스크롤바 없애기
+    $("html,body").css({overflow: "hidden"});
 
-        // 메뉴+인디케이터 이벤트 기능 설정함수 호출 ///
-        autoFn.evtFn();
+    // 스크롤바 위치 최상위
+    window.scrollTo(0,0);
 
-        // 초기화 함수 호출
-        autoFn.initSet();
+    // 자동스크롤 이벤트 설정하기
+    window.addEventListener("wheel", autoFn.wheelFn);
+    // -> window 이벤트 설정을 여기서한 이유는?
+    // ->>> 소멸자를 통해 이벤트를 다른 페이지가 나올때
+    // 해재해 주기 위해 여기서 셋팅함!
 
-        // 페이지번호 초기화 함수 호출
-        autoFn.zeroPno();
+    // 메뉴+인디케이터 이벤트 기능 설정함수 호출 ///
+    autoFn.evtFn();
 
+    // 초기화 함수 호출
+    autoFn.initSet();
 
-        // 드래그배너 기능함수 호출하기
-        dragBanner();
+    // 페이지번호 초기화 함수 호출
+    autoFn.zeroPno();
 
-    },[]); ///////// useEffect : 한번만 //////////////
+    // 드래그배너 기능함수 호출하기
+    dragBanner();
+
+    // 컴포넌트 제거시 실행구역
+    return () => {
+      console.log("메인제거됨!!!!");
+
+      // 자동스크롤 이벤트 제거하기
+      window.removeEventListener("wheel", autoFn.wheelFn);
+      // -> 이 구역에서 window이벤트 설정을 했으므로
+      // 등록된 함수와 동일한 이름으로 셋팅된 함수를 해제할 수 있다!
+
+      // 기존 이벤트 제거함수
+      autoFn.removeEvtFn();
+    }; /// 제거시 실행구역 ///
+  }, []); ///////// useEffect : 한번만 //////////////
 
   // 리턴 코드구역 //////////////
   return (
     <>
       {/* 1. 배너 페이지 */}
-      <section
-        id="ban"
-        className="page none-sel"
-        style={{ background: "lightblue" }}
-      >
+      <section id="ban" className="page none-sel" style={{background: "lightblue"}}>
         {/* 배너모듈 */}
         <Banner />
       </section>
