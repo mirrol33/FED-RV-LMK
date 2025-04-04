85015a02a86142f5d3b73f0994c9de785696f033
freelife11 <mirrol33@gmail.com>
2025-04-04 12:03:43 +0900

    04.04 update3

--- 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/GList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/GList.jsx
@@ -195,14 +195,28 @@ function GList() {
       .siblings().removeClass("on");
     });
 
+    // 만약 처음에 gMode값이 'F'가 아닌경우 'F'로 셋팅하기
+    if (myCon.gMode !== "F") {
+      myCon.setGMode("F");
+    }
+
+    // 홈버튼 첫 페이지로 이동기능 셋팅하기
+    $("#logo a").on("click", (e) => {
+      e.preventDefault();
+      // 라우터이동 : 첫페이지로!
+      myCon.goPage("/");
+    }); //////// click ////////
+
     // 소멸시 이벤트 제거하기 ///
     return(()=>{
       $(".gnb li").off("click");
+      $("#logo a").off("click");
     });
   }, []); //// 처음 한번만 실행 //////
 
-  // ★★★★★★★★★★★★ ///
-  // 코드 리턴구역 /////////////
+  // ★★★★★★★★★★★★ /////////////////
+  // 코드 리턴구역 ///////////////////////////
+  // ★★★★★★★★★★★★ /////////////////
   return (
     <main id="cont">
       <h1 className="tit">All ITEMS List</h1>
