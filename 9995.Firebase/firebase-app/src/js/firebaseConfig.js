7f5d4d841a0fbf7a3b1757045f59657b9c042dd1
LMK <mirrol33@gmail.com>
2025-05-09 16:45:34 +0900

    05.09 update7

--- 9995.Firebase/firebase-app/src/js/firebaseConfig.js
+++ 9995.Firebase/firebase-app/src/js/firebaseConfig.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Firebase 앱 초기화
@@ -24,3 +24,5 @@ export const db = getFirestore(app);
 // getFirestore는 Firestore 데이터베이스에 대한 인스턴스를 가져오는 함수입니다.
 // Firestore는 Firebase의 NoSQL 클라우드 데이터베이스입니다.
 // 이것만 내보내면 내가 만든 DB와 연결하여 테이블을 사용할 수 있다!
+
+console.log('나나',firebaseConfig);
